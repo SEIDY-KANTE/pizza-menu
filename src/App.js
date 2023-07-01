4c21e1491a78c580da743d6527f0ec112a6ed33a
SEIDY-KANTE <82980518+SEIDY-KANTE@users.noreply.github.com>
2023-07-01 19:16:07 +0300

    updating

--- src/App.js
+++ src/App.js
@@ -20,31 +20,40 @@ function Header() {
   );
 }
 function Menu() {
-  console.log(pizzaData);
+  //console.log(pizzaData);
+  const numPizzas = pizzaData.length;
   return (
     <main className="menu">
       <h2>Our menu</h2>
-      <Pizza
+      {numPizzas > 0 && (
+        <ul className="pizzas">
+          {pizzaData.map((pizza, i) => {
+            console.log(i);
+            return <Pizza pizzaObj={pizza} key={i} />;
+          })}
+        </ul>
+      )}
+      {/* <Pizza
         name="Pizza Focaccia"
         ingredients="Bread with italian olive oil and rosemary"
         imgUrl="pizzas/focaccia.jpg"
         price={10}
       />
-     
+      */}
     </main>
   );
 }
 
 function Pizza(props) {
   return (
-    <div>
-      <img src={props.imgUrl} alt={props.name} />
+    <li className="pizza">
+      <img src={props.pizzaObj.imgUrl} alt={props.pizzaObj.name} />
       <div>
-        <h3>{props.name}</h3>
-        <p>{props.ingredients}</p>
-        <span>{props.price}</span>
+        <h3>{props.pizzaObj.name}</h3>
+        <p>{props.pizzaObj.ingredients}</p>
+        <span>{props.pizzaObj.price}</span>
       </div>
-    </div>
+    </li>
   );
 }
 
@@ -53,11 +62,29 @@ function Footer() {
   const openHour = 12;
   const closeHour = 23;
   const isOpen = hour >= openHour && hour <= closeHour;
-  const message = isOpen ? "We're currently open" : "Closed";
+
   return (
     <footer className="footer">
-      {new Date().toLocaleTimeString()}. {message}
+      {isOpen ? (
+        <Order openHour={openHour} closeHour={closeHour} />
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
+      )}
     </footer>
+  );
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're open from {props.openHour}:00 to {props.closeHour}:00. Come visit
+        us or order online.
+      </p>
+      <button className="btn">Order</button>
+    </div>
   );
 }
 
